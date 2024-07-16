5a137b2b943ef2e8f141244d56f12b48f4dabbd1
AmirHossein Norbakhsh <amircr7h@icloud.com>
2024-07-16 23:57:49 +0330

    developed header completely and added fonts

---
+++ src/components/atoms/PrimaryButton.tsx
@@ -0,0 +1,13 @@
+import Image, { StaticImageData } from 'next/image';
+import React from 'react';
+
+const PrimaryButton = ({ text, icon }: { text: string, icon: StaticImageData }) => {
+    return (
+        <button className='bg-[#A259FF] flex flex-row-reverse gap-3 px-8 py-4 rounded-2xl items-center'>
+            <p> {text} </p>
+            <Image src={icon} alt='icon' />
+        </button>
+    );
+};
+
+export default PrimaryButton;
---
+++ src/components/organisms/Header.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import storeFront from '@/assets/icons/Storefront.svg'
+import NFT from '@/assets/images/NFT Marketplace.png'
+import Image from 'next/image';
+import localFont from 'next/font/local'
+import PrimaryButton from '../atoms/PrimaryButton';
+const WorkSans = localFont({ src: '../../assets/fonts/WorkSans-SemiBold.ttf' })
+import user from '@/assets/icons/User.svg'
+
+const Header = () => {
+    const headerItems = ['Marketplace', 'Rankings', 'Connect a wallet']
+    return (
+        <div className={`${WorkSans.className} Header text-white flex flex-row justify-between w-full items-center`}>
+            <div className='Header__leftSide flex flex-row items-center gap-3'>
+                <Image src={storeFront} alt='storeFront' className='cursor-pointer' />
+                <Image src={NFT} alt='NFT Marketplace' className='translate-y-0.5 cursor-pointer' unoptimized />
+            </div>
+            <div className='flex flex-row gap-14 items-center'>
+                {headerItems.map((item, index) => {
+                    return (
+                        <p key={index} className='cursor-pointer'>
+                            {item}
+                        </p>
+                    )
+                })}
+                <PrimaryButton text='Sign Up' icon={user} />
+
+            </div>
+        </div>
+    );
+};
+
+export default Header;
