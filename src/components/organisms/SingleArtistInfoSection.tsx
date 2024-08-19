c39c30c13d95014932f1efacb878e90e6626c925
nou2177 <nou2177@HOS1598_SIS-3>
2024-08-19 11:04:21 +0330

    developed the right side of the single artist info page

--- src/components/organisms/SingleArtistInfoSection.tsx
+++ src/components/organisms/SingleArtistInfoSection.tsx
@@ -7,6 +7,9 @@ import twitter from '@/assets/icons/TwitterLogo.svg'
 import instagram from '@/assets/icons/InstagramLogo.svg'
 import pic from '@/assets/images/Avatar6.png'
 import ArtistInfo from './ArtistInfo';
+import PrimaryButton from '../atoms/PrimaryButton';
+import copy from '@/assets/icons/Copy.svg'
+import plus from '@/assets/icons/Plus.svg'
 
 const SingleArtistInfoSection = () => {
 
@@ -24,14 +27,23 @@ const SingleArtistInfoSection = () => {
                 <Image src={bgPic} alt='background' className=' w-full h-80 object-cover' unoptimized />
             </div>
 
-            <div className='SingleArtistInfoSection__infos bg-[#2B2B2B] flex flex-row items-start px-24'>
+            <div className='SingleArtistInfoSection__infos bg-[#2B2B2B] flex flex-row items-start px-24 w-full justify-between'>
                 <div className='SingleArtistInfoSection__infos__leftSide relative'>
                     <ArtistInfo profilePic={pic} name='Animakid' volume={'250k'} soldNfts={50} followers={300} bio="The internet's friendliest designer kid."
                         links={socialMedias}
                     />
                 </div>
 
-                
+                <div className='SingleArtistInfoSection__infos__rightSide text-white flex flex-row gap-4 mt-20'>
+                    <PrimaryButton text='0xc0E3...B79C' hasIcon icon={copy} />
+                    <button className='flex flex-row-reverse items-center px-8 py-4 gap-2 bg-transparent border-2 border-[#A259FF] rounded-3xl'>
+                        <p>
+                            Follow
+                        </p>
+                        <Image src={plus} alt='pus'/>
+                    </button>
+                </div>
+
             </div>
         </div>
     );
