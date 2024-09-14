bc57de8a9710922e4a2ab88adeb7d1231d82bd97
AmirHossein Norbakhsh <amircr7h@icloud.com>
2024-09-15 00:26:35 +0330

    minor input outline style issue fix

--- src/components/molecules/SubscribeForm.tsx
+++ src/components/molecules/SubscribeForm.tsx
@@ -8,7 +8,7 @@ const WorkSans = localFont({ src: '../../assets/fonts/WorkSans-SemiBold.ttf' })
 const SubscribeForm = ({ placeholder, bgColor, hasIcon }: { placeholder: string, bgColor: string, hasIcon: boolean }) => {
     return (
         <div className={` ${WorkSansRegular.className} SubscribeForm relative w-[95%]`}>
-            <input placeholder={placeholder} className='rounded-2xl bg-white text-[#2B2B2B] placeholder:text-[#2B2B2B]  w-full
+            <input placeholder={placeholder} className='rounded-2xl outline-none bg-white text-[#2B2B2B] placeholder:text-[#2B2B2B]  w-full
             py-5 px-4 ' />
             <button className={`bg-[${bgColor}]  rounded-2xl flex flex-row-reverse items-center py-5 px-10 gap-3 absolute right-0 top-0`}>
                 <p className={`${WorkSans.className}`}>
