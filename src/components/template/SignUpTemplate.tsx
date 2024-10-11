d2d45247cc8d24b6d66080a1e31eeaa2f19e122c
AmirHossein Norbakhsh <amircr7h@icloud.com>
2024-10-11 15:45:14 +0330

    Test ESLint no-undef rule

--- src/components/template/SignUpTemplate.tsx
+++ src/components/template/SignUpTemplate.tsx
@@ -8,7 +8,6 @@ const SignUpTemplate = () => {
     return (
         <main className="flex min-h-screen flex-col items-center pt-8 bg-[#2B2B2B] justify-between">
             <div className="xl:px-28 lg:px-20 sm:px-14 px-8 w-full">
-            <a href="/about">Go to About Page</a>
                 <Header />
             </div>
             <CreateAccount />
