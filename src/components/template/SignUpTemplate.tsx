62eca55a756b0c74cfe6df98cc987a0f1845c97a
AmirHossein Norbakhsh <amircr7h@icloud.com>
2024-10-11 15:37:35 +0330

    testing husky  pre-commit 2

--- src/components/template/SignUpTemplate.tsx
+++ src/components/template/SignUpTemplate.tsx
@@ -5,11 +5,10 @@ import Header from "@/components/organisms/Header";
 
 const SignUpTemplate = () => {
     
-    
-    console.log(test2);
     return (
         <main className="flex min-h-screen flex-col items-center pt-8 bg-[#2B2B2B] justify-between">
             <div className="xl:px-28 lg:px-20 sm:px-14 px-8 w-full">
+            <a href="/about">Go to About Page</a>
                 <Header />
             </div>
             <CreateAccount />
