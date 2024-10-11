79d3395e770e2e9b8418bb066068d804a535caf2
AmirHossein Norbakhsh <amircr7h@icloud.com>
2024-10-11 15:33:37 +0330

    added husky for pre-commit checks and testing it

--- src/components/template/SignUpTemplate.tsx
+++ src/components/template/SignUpTemplate.tsx
@@ -4,6 +4,8 @@ import Footer from "@/components/organisms/Footer";
 import Header from "@/components/organisms/Header";
 
 const SignUpTemplate = () => {
+    
+    console.log(test);
     return (
         <main className="flex min-h-screen flex-col items-center pt-8 bg-[#2B2B2B] justify-between">
             <div className="xl:px-28 lg:px-20 sm:px-14 px-8 w-full">
